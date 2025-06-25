1408fd995ceb2f83b8b19d30ff4687b7ec1a62fa
kavyasree2510 <kavyareddie2510@gmail.com>
2025-06-25 11:31:27 +0530

    lab 2

---
+++ lab 2.js
@@ -0,0 +1,16 @@
+let adjective = "fierce";
+let noun = "dragon";
+let verb = "angry";
+let place = "volcano";
+let adjective2 = "fiery";
+let noun2 = "marshmallows";
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+adjective = "silly";
+noun = "unicorn";
+verb = "tickled";
+place = "rainbow forest";
+adjective2 = "sparkly";
+noun2 = "cupcakes";
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
